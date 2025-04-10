71b64ee62321deff84dc28c9ece5c9e625443382
thomas rousseau <thomas.rousseau19@gmail.com>
2025-04-10 10:51:27 +0200

    betty style

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -10,8 +10,7 @@
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 if (!func || !tree)
-    return;
-    
+return;
 func(tree->n);
 binary_tree_preorder(tree->left, func);
 binary_tree_preorder(tree->right, func);
