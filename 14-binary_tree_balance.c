9d854d368586bc5929bbcfc9ee28fbb85ab10e90
thomas rousseau <thomas.rousseau19@gmail.com>
2025-04-11 10:09:10 +0200

    r

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,24 +1,37 @@
 #include "binary_trees.h"
-#include <stdlib.h>
+/**
+ * balance_height - Function that finds the height of a node.
+ * @tree: It's a pointer that pointe the root node.
+ * Return: return the height in end.
+ */
+
+int balance_height(binary_tree_t *tree)
+{
+int height_left, height_right;
+
+if (tree == NULL)
+{
+return (0);
+}
+height_left = balance_height(tree->left);
+height_right = balance_height(tree->right);
+
+return (1 + (height_left > height_right ? height_left : height_right));
+}
 
 /**
- * binary_tree_balance - v
- * @tree: Pointeur l'arbre
- * Return: size_t
-*/
+ * binary_tree_balance - Function that measures the balance
+ * factor of a binary tree.
+ * @tree: It's a pointer that pointe to the root node of
+ * the tree to measure the balance factor.
+ * Return: return the balance factor in end.
+ */
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-int left = 0, right = 0;
-
-if (!tree)
+if (tree == NULL)
+{
 return (0);
-
-if (tree->left != NULL)
-left = 1 + binary_tree_balance(tree->left);
-
-if (tree->right  != NULL)
-right = 1 + binary_tree_balance(tree->right);
-
-return (left - right);
+}
+return (balance_height(tree->left) - balance_height(tree->right));
 }
--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,5 +9,20 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+binary_tree_t *parent;
+if (node == NULL)
+return (NULL);
 
+if (node->parent == NULL)
+return (NULL);
+
+parent = node->parent;
+
+if (parent->left == node && parent->right)
+return (parent->right);
+
+if (parent->right == node && parent->left)
+return (parent->left);
+
+return (NULL);
 }
